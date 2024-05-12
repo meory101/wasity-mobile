66d68c0cb9c084a4e54c46c44f342929790883b5
meory101 <othmannour2001@gmail.com>
2024-05-12 06:55:06 +0300

    init project

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import 'package:wasity/core/resource/size_manager.dart';
-
+//
 class App extends StatelessWidget {
   const App({super.key});
 
